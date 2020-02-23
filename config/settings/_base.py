8a9ec3634f8013cc54dc73cfffcc5ff3242aed78
phc613 <pejmanhadavi77@gmail.com>
2020-02-24 01:30:31 +0330

    Fix static and media files bug and add static files.

--- config/settings/_base.py
+++ config/settings/_base.py
@@ -92,8 +92,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = '../media'
