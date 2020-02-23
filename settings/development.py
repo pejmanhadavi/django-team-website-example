db89e01c83ed2b3e39ded95b17725f121fbae65d
phc613 <pejmanhadavi77@gmail.com>
2020-02-23 21:05:34 +0330

    Add settings folder and some environment for development and deployment

--- settings/development.py
+++ settings/development.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'app_config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
