34aafb9665a14115cdc8d35746695caa680150c5
phc613 <pejmanhadavi77@gmail.com>
2020-02-24 15:18:06 +0330

    Add about template

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
