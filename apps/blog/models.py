5f2727863ce9f3f8fc5b4f825bd066fe53baca4d
phc613 <pejmanhadavi77@gmail.com>
2020-02-26 16:00:33 +0330

    Add urls for categories and posts.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from ckeditor_uploader.fields import RichTextUploadingField
 from image_cropping import ImageRatioField
--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,6 +6,6 @@ from apps.blog.views import ArticleListView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
-    path('single/', TemplateView.as_view(template_name='article_single.html'), name='article_single'),
-    path('category/', TemplateView.as_view(template_name='article_list.html'), name='article_category')
+    path('<slug:slug>/', TemplateView.as_view(template_name='article_single.html'), name='article_single'),
+    path('category/<slug:slug>/', TemplateView.as_view(template_name='article_list.html'), name='article_category')
 ]
