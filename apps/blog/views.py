97ab0102bd488114710ef07f6f98f80bb20aaded
phc613 <pejmanhadavi77@gmail.com>
2020-02-26 15:39:25 +0330

    Add pagination for articles

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,3 +7,11 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'article_list.html'
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleListView, self).get_context_data(**kwargs)
+        context.update({
+            'category_list': Category.objects.order_by('name'),
+        })
+        return context
